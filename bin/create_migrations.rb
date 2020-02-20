918446d4f1dcc3ec68989fb4e52aa926e3017efa
Brian Buchalter <brian.buchalter@gusto.com>
2020-02-19 19:47:56 -0700

    Remove initial migration to reduce demo noise

--- bin/create_migrations.rb
+++ bin/create_migrations.rb
@@ -3,10 +3,9 @@
 require 'pathname'
 require 'securerandom'
 
-current_file_path = Pathname.new('db/migrate/20200219200125_create_books.rb')
+current_date = 20200219200125
 
 400.times do
-  current_date = Integer(current_file_path.basename.to_s.split('_').first)
   next_date = current_date + 1
   klass_name_randomization = ('a'..'z').to_a.shuffle[0,8].join
   next_file_path = Pathname.new("db/migrate/#{next_date}_create_books#{klass_name_randomization}.rb")
@@ -26,4 +25,5 @@ current_file_path = Pathname.new('db/migrate/20200219200125_create_books.rb')
   MIGRATION
   File.write(next_file_path, next_file_content)
   current_file_path = next_file_path
+  current_date = next_date
 end
